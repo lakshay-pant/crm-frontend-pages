b5a2c591620869131b8a1ea6c2abb73d9e570682
Lakshay <pantlakshay2@gmail.com>
2021-02-10 16:53:45 +0530

    add student

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import {AddStudent} from "./pages/new-student/AddStudent.page"
+
+
+function App() {
+  return( 
+    <div className="App">
+    <AddStudent/>
+      </div>
+  
+  )}
+
+export default App;
---
+++ src/components/add-student-form/addStudentForm.comp.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const addStudentForm = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
